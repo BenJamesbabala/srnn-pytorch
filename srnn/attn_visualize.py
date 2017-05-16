601a5e19fc1ba6cc0f76b3275cf958cfa46c508c
Anirudh Vemula <vvanirudh@gmail.com>
2017-05-16 14:35:21 -0400

    Attention visualize script incomplete

--- srnn/attn_visualize.py
+++ srnn/attn_visualize.py
@@ -7,6 +7,7 @@ Date: 8th May 2017
 import numpy as np
 import matplotlib.pyplot as plt
 import pickle
+import ipdb
 
 
 def plot_attention(true_pos_nodes, pred_pos_nodes, nodes_present, observed_length, attn_weights, name, plot_directory):
@@ -25,14 +26,39 @@ def plot_attention(true_pos_nodes, pred_pos_nodes, nodes_present, observed_lengt
                 traj_data[ped][0].append(true_pos[ped, :])
                 traj_data[ped][1].append(pred_pos[ped, :])
 
-    for j in traj_data:
-        # For each ped
-        for tstep in range(traj_length):
-            if tstep < observed_length:
-                # Observed part
-                continue
-            # Predicted part
-            
+    for tstep in range(traj_length):
+        if tstep < observed_length:
+            # Observed part
+            continue
+        # Predicted part
+        # Create a plot for current prediction tstep
+        # TODO for now, just plot the attention for the first step
+        for ped in nodes_present[tstep]:
+            true_traj_ped = np.array(traj_data[ped][0])
+            pred_traj_ped = np.array(traj_data[ped][1])
+
+            peds_other = attn_weights[tstep-observed_length][ped][1]
+            attn_w = attn_weights[tstep-observed_length][ped][0]
+
+            plt.figure()
+            c = 'r'
+            # ipdb.set_trace()
+            plt.plot(true_traj_ped[:, 0], true_traj_ped[:, 1], color=c, linestyle='solid', marker='o', linewidth=1)
+            plt.plot(pred_traj_ped[:, 0], pred_traj_ped[:, 1], color=c, linestyle='dashed', marker='x', linewidth=1)
+
+            for ind_ped, ped_o in enumerate(peds_other):
+                true_traj_ped_o = np.array(traj_data[ped_o][0])
+                pred_traj_ped_o = np.array(traj_data[ped_o][1])
+
+                weight = attn_w[ind_ped]
+
+                c = np.random.rand(3, 1)
+                plt.plot(true_traj_ped_o[:, 0], true_traj_ped_o[:, 1], color=c, linestyle='solid', marker='o', linewidth=2*weight)
+                plt.plot(pred_traj_ped_o[:, 0], pred_traj_ped_o[:, 1], color=c, linestyle='dashed', marker='x', linewidth=2*weight)
+
+            plt.show()
+
+        break
 
 
 def main():
