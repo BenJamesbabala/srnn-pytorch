0d5ba47f2be1774ae9403a78939d9bac7d5598e5
Anirudh Vemula <vvanirudh@gmail.com>
2017-06-24 14:22:19 -0400

    Added script to make directories

---
+++ make_directories.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+mkdir save
+mkdir log
+
+cd save
+mkdir 0 1 2 3 4
+cd 0
+mkdir save_attention
+cd ../1
+mkdir save_attention
+cd ../2
+mkdir save_attention
+cd ../3
+mkdir save_attention
+cd ../4
+mkdir save_attention
+
+cd ../../log
+mkdir 0 1 2 3 4
+cd 0
+mkdir log_attention
+cd ../1
+mkdir log_attention
+cd ../2
+mkdir log_attention
+cd ../3
+mkdir log_attention
+cd ../4
+mkdir log_attention
+
+cd ../../
