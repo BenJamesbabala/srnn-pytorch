d6e886ddff391c43dacd89a760d4d96481239bef
Anirudh Vemula <vvanirudh@gmail.com>
2017-06-24 00:43:07 -0400

    Change

--- srnn/train.py
+++ srnn/train.py
@@ -26,7 +26,7 @@ def main():
     # RNN size
     parser.add_argument('--human_node_rnn_size', type=int, default=256,
                         help='Size of Human Node RNN hidden state')
-    parser.add_argument('--human_human_edge_rnn_size', type=int, default=128,
+    parser.add_argument('--human_human_edge_rnn_size', type=int, default=256,
                         help='Size of Human Human Edge RNN hidden state')
 
     # Input and output size
@@ -40,7 +40,7 @@ def main():
     # Embedding size
     parser.add_argument('--human_node_embedding_size', type=int, default=64,
                         help='Embedding size of node features')
-    parser.add_argument('--human_human_edge_embedding_size', type=int, default=64,
+    parser.add_argument('--human_human_edge_embedding_size', type=int, default=128,
                         help='Embedding size of edge features')
 
     parser.add_argument('--attention_size', type=int, default=64,
@@ -167,7 +167,7 @@ def train(args):
     for epoch in range(args.num_epochs):
         # optimizer = torch.optim.RMSprop(net.parameters(), lr=learning_rate)
         for param_group in optimizer.param_groups:
-            param_group['lr'] = learning_rate
+	    param_group['lr'] = learning_rate
 
         learning_rate *= args.decay_rate
         # learning_rate = args.learning_rate / np.sqrt(epoch + 1)
